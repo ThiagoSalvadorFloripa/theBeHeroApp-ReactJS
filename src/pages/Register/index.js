cd294d8c0234574f8524b6b927a494bb656a8f4b
Salvador-nitro <Salvador-nitro>
2020-04-04 16:01:07 -0300

    pagina register sem css

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -23,7 +23,16 @@ export default function Register(){
                 </section>
 
                 <form>
-                    
+                    <input placeholder="Nome da Ong"/>
+                    <input type="email" placeholder = "E-mail"/>
+                    <input placeholder="WhatsApp"/>
+
+                    <div className ="input-group">
+                        <input placeholder="Cidade"/>
+                        <input placeholder="UF" style = {{ width :80}}/>
+                    </div>
+
+                    <button className = "button" type = "submit"> Cadastrar </button>
                 </form>
             </div>
         </div>
