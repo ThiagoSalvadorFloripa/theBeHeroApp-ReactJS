6dcbf04ff2711fd794fab2ff3d39c4a725adb730
Salvador-nitro <Salvador-nitro>
2020-04-08 15:52:27 -0300

    finalizando front

---
+++ src/pages/NewIncident/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+export default function Profile(){
+    return (
+        <div className = "newIncident-container">
+            <div className = "content">
+                <section>
+                    <img src = {logoImg} alt = "Be The Hero" />
+
+                    <h1>Cadastrar Novo Caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para lhe ajudar</p>
+
+                    <Link className = "back-link"  to= "/">
+                        <FiArrowLeft size = {16} color = "#E02041"/>
+                        Voltar para Home
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Titulo do Caso"/>
+                    <textarea placeholder = "Descrição"/>
+                    <input type="number" number placeholder="Valor em reais"/>
+                        
+
+                    <button className = "button" type = "submit"> Cadastrar </button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -11,7 +11,7 @@ return (
         <header>
             <img src = {logoImg} alt=" Be The Hero"/>
             <span>Bem vinda, APAD</span>
-            <Link className = 'button' to = "/inicident/new">Cadastrar novo caso</Link>
+            <Link className = 'button' to = "/incident/new">Cadastrar novo caso</Link>
             <button type = 'button'>
                 <FiPower size = {18} color = "#E02041"/>
             </button>
