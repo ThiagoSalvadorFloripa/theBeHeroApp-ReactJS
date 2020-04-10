7ce3cccaec033392829db8d0dea2fb59dd113c10
TSalvador <TSalvador>
2020-04-10 14:39:23 -0300

    conectando e validando Logon com Back

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
+
 
 export default function Logon() {
+  const [id, setId] = useState(''); // começa com campo em branco
+  const history = useHistory();
+
+  async function handleLogon(e) {
+    e.preventDefault(); //para não da reload na pagina
+
+    try {
+      const response = await api.post('session', {id});
+      
+      // para ficar disponivel em toda aplicação será salvo no storege
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      //envia para outra rota
+      history.push('/profile');
+      
+      console.log(response.data.name);
+    } catch (error) {
+      alert('Fallha no Login, tente novamente.');
+      
+    }
+    
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
 
         <img src = {logoImg} alt = "Be The Hero" />
 
-        <form>
+        <form onSubmit = {handleLogon}>
           <h1>Faça seu Login</h1>
 
-          <input placeholder = "Seu ID"/>
+          <input placeholder = "Seu ID"
+          value = { id}
+          onChange = {e => setId(e.target.value)}
+          />
 
           <button className="button" type= "submit">Entrar</button>
 
